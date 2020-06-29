0b258b36e32ddf9783ed7a5e70171ee698bb823c
F7East <ti602@nyu.edu>
2020-06-29 04:11:38 +0400

    corrected graph path, coreNFW, lg, t_sf.

--- DM_Profiles.py
+++ DM_Profiles.py
@@ -3,6 +3,7 @@ from scipy.optimize import curve_fit as fit
 from scipy.stats import chisquare
 from pynbody import units
 import pynbody as pyn
+from numpy import log10 as lg
 
 class model:
     
@@ -40,12 +41,12 @@ class model:
     
     '''
     
-    def __init__(self, profile, stellar_mass, halo_mass, shm_radius, r_200, name, pmodel = 'pISO'):
+    def __init__(self, profile, stellar_mass, halo_mass, shm_radius, r_200, t_sf, name, pmodel = 'pISO'):
 
        
         # radii and density
         self.radii = profile['rbins']
-        self.log_den = numpy.log10(profile['density'])
+        self.log_den = lg(profile['density'])
         
         #errors in density
         self.den_error = []
@@ -77,38 +78,40 @@ class model:
         
         #Einasto coefficients
         if self.pmodel == 'Einasto_ae':
-            halo_mass.convert_units(units.h*units.Msol*10**12)
-            m = numpy.log10(float(halo_mass))
+            halo_mass.in_units(units.h*units.Msol*10**12)
+            m = lg(halo_mass)
             ν = 10**(-0.11 + 0.146*m + 0.0138*m**2 + 0.00123*m**3)
             self.alpha_e = (0.0095*ν**2 + 0.155)
             
         #DC14 coefficients
         if self.pmodel == 'DC14':
-            X = numpy.log10(stellar_mass/halo_mass)
-            self.alpha = 2.94 - numpy.log10(10**((X+2.33)*(-1.08)) + 10**((X+2.33)*2.29))
+            X = lg(stellar_mass/halo_mass)
+            self.alpha = 2.94 - lg(10**((X+2.33)*(-1.08)) + 10**((X+2.33)*2.29))
             self.beta  = 4.23 + 1.34*X + 0.26*X**2
-            self.gamma = -0.06 + numpy.log10(10**((X+2.56)*(-0.68)) + 10**(X+2.56))
+            self.gamma = -0.06 + lg(10**((X+2.56)*(-0.68)) + 10**(X+2.56))
             
             
         #coreNFW coefficients
         
         if self.pmodel == 'coreNFW':
             self.G = float(units.G.ratio(profile['rbins'].units**3*profile['mass'].units**-1*units.Gyr**-2))
+            self.t_sf = t_sf
         
         
         #fitting with different number of parameters
         if self.pmodel == 'Einasto':
-            self.initial_guess = [self.log_den[0], 0.001, 1]
+            self.initial_guess = [self.log_den[0], 1, 1]
             self.bounding = ([self.log_den[-1], 0, -numpy.inf] , numpy.inf)
+            
         elif self.pmodel == 'coreNFW':
-            self.initial_guess = [self.log_den[0], 0.001, 2., 0.1]
+            self.initial_guess = [self.log_den[0], 1, 1.75, 0.04]
             self.bounding = ([self.log_den[-1], 0, 0, 0] , numpy.inf)
             
         else:
-            self.initial_guess = [self.log_den[0], 0.001]
+            self.initial_guess = [self.log_den[0], 1]
             self.bounding = ([self.log_den[-1], 0] , numpy.inf)
         
-        self.params, self.covar = fit(self.log_rho, self.radii, self.log_den, sigma = self.log_den_error, absolute_sigma =  True, p0  = self.initial_guess, bounds = self.bounding, maxfev = 600)
+        self.params, self.covar = fit(self.log_rho, self.radii, self.log_den, sigma = self.log_den_error, absolute_sigma =  True, p0  = self.initial_guess, bounds = self.bounding, maxfev = 1000)
         
         self.C_200 = self.r_200 / self.params[1]
         
@@ -130,19 +133,19 @@ class model:
         '''
         
         if self.pmodel == 'pISO':
-            return log_rho_s - numpy.log10((1+(r/r_s)**2))
+            return log_rho_s - lg((1+(r/r_s)**2))
         if self.pmodel == 'Burket':
-            return log_rho_s - numpy.log10((1+(r/r_s)**2)) - numpy.log10(1+(r/r_s))
+            return log_rho_s - lg((1+(r/r_s)**2)) - lg(1+(r/r_s))
         if self.pmodel == 'NFW':
             return self.NFW(r, log_rho_s, r_s)
         if self.pmodel == 'Lucky13':
-            return log_rho_s - numpy.log10(1+(r/r_s))**3
+            return log_rho_s - lg(1+(r/r_s))**3
         if self.pmodel == 'Einasto_ae':
             return log_rho_s - 2/(numpy.log(10)*self.alpha_e)*((r/r_s)**self.alpha_e-1)
         if self.pmodel == 'Einasto':
             return log_rho_s - 2/(numpy.log(10)*args[0])*((r/r_s)**args[0])
         if self.pmodel == 'DC14':
-            return log_rho_s - self.gamma*numpy.log10(r/r_s) - (self.beta - self.gamma)/self.alpha*numpy.log10(1+(r/r_s)**self.alpha)
+            return log_rho_s - self.gamma*lg(r/r_s) - (self.beta - self.gamma)/self.alpha*lg(1+(r/r_s)**self.alpha)
         if self.pmodel == 'coreNFW':
             return self.coreNFW(r, log_rho_s, r_s, args[0], args[1])
         
@@ -158,7 +161,7 @@ class model:
         return etta*self.shm_radius
     
     def NFW(self, r, log_rho_s, r_s ):
-        return log_rho_s - numpy.log10(1+(r/r_s))*2 - numpy.log10(r/r_s)
+        return log_rho_s - lg(1+(r/r_s))*2 - lg(r/r_s)
     
     def M_NFW(self, r, log_rho_s, r_s):
         return 4*numpy.pi*(10**log_rho_s)*(r_s**3)*(numpy.log(1+r/r_s)+(r/r_s)/(1+r/r_s))
@@ -166,16 +169,15 @@ class model:
     def f(self, r, etta):
         return numpy.tanh(r/self.r_c(etta))
     
-    def n(self, r, log_rho_s, r_s, k):
-        t_sf = 14.
-        return numpy.tanh(k*t_sf/self.t_dyn(log_rho_s, r_s))
+    def n(self, log_rho_s, r_s, k):
+        return numpy.tanh(k*self.t_sf/self.t_dyn(log_rho_s, r_s))
     
     def t_dyn(self, log_rho_s, r_s):
         return 2*numpy.pi*numpy.sqrt(r_s**3/(self.G*self.M_NFW(r_s, log_rho_s, r_s)))
         # question: M_NFW(r_s) means M_NFW(r_s, log_rho_s, r_s)? probably yes
-    
+
     def coreNFW(self, r, log_rho_s, r_s, etta, k):
-        return numpy.log10(self.f(r, etta)**self.n(r, log_rho_s, r_s, k)*10**self.NFW(r, log_rho_s, r_s)+self.n(r, log_rho_s, r_s, k)*self.f(r, etta)**(self.n(r, log_rho_s, r_s, k)-1.)*(1.-self.f(r, etta)**2)*self.M_NFW(r, log_rho_s, r_s)/(4*numpy.pi*r**2*self.r_c(etta)))
+        return  (2*self.n( log_rho_s, r_s, k)-1)*lg(self.f(r,etta)) + self.NFW(r, log_rho_s, r_s) + lg(self.M_NFW(r, log_rho_s, r_s)) +  lg(self.n( log_rho_s, r_s, k)) + lg(1-self.f(r, etta)**2) - lg(numpy.pi*4*self.r_c(etta)) - 2*lg(r)
         
         
         
